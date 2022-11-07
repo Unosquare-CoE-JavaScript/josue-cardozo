6901522e871daacef04115c34e35b368f987a71e
Josue cardozo <josue.cardozo@unosquare.com>
2022-11-07 16:22:10 -0400

    Module 1.6

---
+++ Module 1.6/AdvancedNodeStarter/config/dev.js
@@ -0,0 +1,11 @@
+module.exports = {
+  googleClientID:
+    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+  //mongoURI: 'mongodb://josue:admin123@ac-tf2na4r-shard-00-00.oy5xbzb.mongodb.net:27017,ac-tf2na4r-shard-00-01.oy5xbzb.mongodb.net:27017,ac-tf2na4r-shard-00-02.oy5xbzb.mongodb.net:27017/?ssl=true&replicaSet=atlas-xubzr8-shard-0&authSource=admin&retryWrites=true&w=majority',
+  mongoURI: 'mongodb://localhost/blogster',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379',
+  accesKeyId: 'AKIAUERBXEHWYLNQCV4I',
+  secretAccessKey: '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'
+};
